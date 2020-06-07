ab76787892db8e07b3f6c1bcf97e49730ffaf3ad
Neeff <dpincheira2016@alu.uct.cl>
2020-06-07 04:23:23 -0400

    add: edit model Sale

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,30 +1,27 @@
 class Sale < ApplicationRecord
 
-def self.deals_ar
-  where(currency: 'ARS')
-end
-
-def self.deals_us
-  where(currency:'USD')
-end
-
-def self.deals_cl
-  where(currency: 'CLP')
-end
-
-def self.count_by_region
-  count_by_region = { }
-  pluck(:currency).uniq.each do |region|
-    count_by_region[region.to_s] = where(currency: region).count
+  def self.deals_ar
+    where(currency: 'ARS')
   end
-  count_by_region
-end
 
-def self.group_by_name
-  all.group_by(&:name_game)
-end
+  def self.deals_us
+    where(currency:'USD')
+  end
 
+  def self.deals_cl
+    where(currency: 'CLP')
+  end
 
+  def self.count_by_region
+    count_by_region = { }
+    pluck(:currency).uniq.each do |region|
+      count_by_region[region.to_s] = where(currency: region).count
+    end
+   count_by_region
+  end
 
+  def self.group_by_name
+    all.group_by(&:name_game)
+  end
 end
 
